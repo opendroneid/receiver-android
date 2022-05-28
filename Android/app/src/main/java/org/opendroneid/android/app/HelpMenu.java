a0daa018a166efbb0c6b51347c9bbb10360325f4
murata <ma2maru@gmail.com>
2022-05-28 11:40:25 +0900

    app: Localize the wording of java code

--- Android/app/src/main/java/org/opendroneid/android/app/HelpMenu.java
+++ Android/app/src/main/java/org/opendroneid/android/app/HelpMenu.java
@@ -25,28 +25,16 @@ public class HelpMenu extends DialogFragment {
         View view = inflater.inflate(R.layout.help_text, container, true);
 
         TextView helpView = view.findViewById(R.id.bluetoothHelpText);
-        String linkText =
-                "All devices should be able to receive Bluetooth 4 Legacy Advertising remote ID signals.<br><br>" +
-                "To receive Bluetooth 5 Long Range remote ID signals, the device must support the \"Coded Phy\" feature and also the \"Extended Advertising\" feature.<br><br>" +
-                "See the claimed support for this device for these features in the options menu.<br><br>" +
-                "Please be advised that even with those features supported, the device is not necessarily able to pick up the Long Range signals or it will only do that in limited time intervals.<br>";
+        String linkText = getString(R.string.bluetoothHelp);
         helpView.setText(Html.fromHtml(linkText));
 
         helpView = view.findViewById(R.id.beaconHelpText);
-        linkText =
-                "All devices running Android 6 (Marshmallow) or newer should be able to receive Wi-Fi Beacon remote ID signals.<br><br>" +
-                "The reception rate can vary a lot from one device to another, depending on the HW and SW.<br><br>" +
-                "The reception rate for Wi-Fi Beacon remote ID signals can to some extent be improved by disabling Wi-Fi scan <a href='https://developer.android.com/guide/topics/connectivity/wifi-scan#wifi-scan-throttling'>throttling</a>.<br><br>" +
-                "Enable the <a href='https://developer.android.com/studio/debug/dev-options'>Android Developer Mode</a> by opening the Settings application.<br><br>" +
-                "Find the SW information (possibly in the \"About Phone\" menu) and click 7 times on the \"Build Number\".<br><br>" +
-                "Go back, find the \"Developer options\" menu, scroll to \"Wi-Fi scan throttling\" and disable it.<br>";
+        linkText = getString(R.string.beaconHelp);
         helpView.setText(Html.fromHtml(linkText));
         helpView.setMovementMethod(LinkMovementMethod.getInstance());
 
         helpView = view.findViewById(R.id.nanHelpText);
-        linkText =
-                "Reception of Wi-Fi Neighbor-aware Network remote ID signals is possible only if the device has support for Wi-Fi NaN.<br><br>" +
-                "See the claimed support for this device for this feature in the options menu.<br><br>";
+        linkText = getString(R.string.nanHelp);
         helpView.setText(Html.fromHtml(linkText));
         return view;
     }
